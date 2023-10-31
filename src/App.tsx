f4c40f9a08a12a3fae0c71884034d0a3f8e4481a
Ra <rami@jwcorp.fr>
2023-10-31 16:05:02 +0100

    update svg testing background dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import LandingPrincipal from "./Component/LandingPrincipal/LandingPrincipal.tsx";
 import FeaturesPresentation from "./Component/FeaturesPresentation/FeaturesPresentation.tsx";
-
 function App() {
   return (
     <div className="landing-page-container">
