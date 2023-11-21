0029d6c66a63669cb4970a4ba17978db1da1fa5a
Ra <rami@jwcorp.fr>
2023-11-21 01:04:51 +0100

    added multi lang section

--- src/Component/FeaturesPresentation/FeaturesPresentation.tsx
+++ src/Component/FeaturesPresentation/FeaturesPresentation.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./FeaturesPresentation.css";
 import testimonialsImg from "../../Assets/testimonials.svg";
+import multiLang from "../../Assets/multilang.svg";
 function FeaturesPresentation() {
   return (
     <>
@@ -15,14 +16,27 @@ function FeaturesPresentation() {
             venenatis turpis parturient nisl sed
           </div>
         </div>
-        <div className="features-presentation-concept-container">
-          <div className="features-presentation-concept-title">Concept</div>
-          <div className="features-presentation-concept-description">
-            Lorem ipsum dolor u tendamas dolor u tendamas lorem ipsum dolor u
-            tendamas Lorem ipsum dolor u tendamas dolor.
+        <div className="features-presentation-concept-and-multi-lang-container">
+          <div className="features-presentation-concept-container">
+            <div className="features-presentation-concept-title">Concept</div>
+            <div className="features-presentation-concept-description">
+              Lorem ipsum dolor u tendamas dolor u tendamas lorem ipsum dolor u
+              tendamas Lorem ipsum dolor u tendamas dolor.
+            </div>
+            <img src={testimonialsImg} alt="testimonials image" />
+            {/* 1320 */}
           </div>
-          <img src={testimonialsImg} alt="testimonials image" />
-          {/* 1320 */}
+          <div className="features-presentation-multi-lang-container">
+            <div className="features-presentation-multi-lang-title">
+              Multi-langues
+            </div>
+            <div className="features-presentation-multi-lang-description">
+              Lorem ipsum dolor u tendamas dolor
+            </div>
+            <div className="features-presentation-multi-lang-img-container">
+              <img src={multiLang} alt="" />
+            </div>
+          </div>
         </div>
       </div>
     </>
