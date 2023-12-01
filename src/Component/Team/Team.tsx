86256ea84d6d0252dd58f0d7ac96aaa9e3de034e
Ra <rami@jwcorp.fr>
2023-12-01 03:28:28 +0100

    added founder section

--- src/Component/Team/Team.tsx
+++ src/Component/Team/Team.tsx
@@ -15,9 +15,24 @@ function Team() {
       <div className="team-members-bloc-container">
         <div className="team-members-founder-container">
           <div className="team-members-profile-pic-and-name-and-job">
-            <div className="team-members-founder-profile-pic-container"></div>
+            <div className="team-members-founder-profile-pic-container">
+              <img
+                src="https://media.licdn.com/dms/image/C4D03AQF1p1jw4KP5RQ/profile-displayphoto-shrink_200_200/0/1590163254048?e=1707350400&v=beta&t=SlCjnsYeTKKDhYUGidJAmzH919l2cUFFpN-VU2loM4o"
+                alt=""
+              />
+            </div>
             <div className="team-members-founder-name">Rémi Blaise</div>
-            <div className="team-members-founder-job"></div>
+            <div className="team-member-founder-job-container">
+              <div className="team-members-founder-job">Fondateur</div>
+            </div>
+          </div>
+          <div className="team-members-founders-description">
+            Rémi a la vision d’un produit pensé pour les utilisateurs, par les
+            utilisateurs. Avec une conviction forte : si la blockchain est
+            encore austère et abstraite, ce n’est pas une fatalité ! Développeur
+            web depuis le jeune âge, ingénieur, passionné par les startups et la
+            blockchain, cette problèmatique encore; non résolue, est sa
+            principale mission.
           </div>
         </div>
       </div>
