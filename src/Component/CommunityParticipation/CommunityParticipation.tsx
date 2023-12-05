d67275e1b5d35c46b26905c3117870f8f48ceacd
JW Rami <rami@jwcorp.fr>
2023-12-05 22:26:03 +0100

    added questionnaire bloc with image

--- src/Component/CommunityParticipation/CommunityParticipation.tsx
+++ src/Component/CommunityParticipation/CommunityParticipation.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./CommunityParticipation.css";
+import questionnaireImage from "../../Assets/questionnaire.svg";
 function CommunityParticipation() {
   return (
     <div className="community-participation-container">
@@ -12,6 +13,21 @@ function CommunityParticipation() {
           Lorem ipsum dolor sit amet consectetur. Arcu ultricies condimentum
           venenatis turpis parturient nisl sed
         </div>
+      </div>
+      <div className="community-participation-bloc-container">
+        <div className="community-participation-questionnaire-container">
+          <div className="community-participation-questionnaire-title">
+            Donne ton avis en 2 minutes !
+          </div>
+          <div className="community-participation-questionnaire-description">
+            Lorem ipsum dolor u tendamas dolor u tendamas lorem ipsum dolor u
+            tendamas dolor.
+          </div>
+          <div className="community-participation-questionnaire-image-container">
+            <img src={questionnaireImage} alt="questionnaire" />
+          </div>
+        </div>
+        <div className="community-participation-telegram-container"></div>
       </div>
     </div>
   );
