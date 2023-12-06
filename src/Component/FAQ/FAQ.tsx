90c710d46d8645eec05163c9ef7fbfad1f2fa673
Ra <rami@jwcorp.fr>
2023-12-06 19:16:51 +0100

    added question to faq

--- src/Component/FAQ/FAQ.tsx
+++ src/Component/FAQ/FAQ.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
-import './FAQ.css'
+import React, { useState } from "react";
+import chevronUp from "../../Assets/chevron-up.svg";
+import chevronDown from "../../Assets/chevron-down.svg";
+import "./FAQ.css";
 function FAQ() {
+  const [isFirstQuestionClicked, setIsFirstQuestionClicked] = useState(false);
+  function handleQuestionClick() {
+    setIsFirstQuestionClicked(!isFirstQuestionClicked);
+  }
   return (
-    <div className='faq-container'>FAQ</div>
-  )
+    <div className="faq-container">
+      <div className="faq-title">Questions fréquentes</div>
+      <div className="faq-description">
+        Learn more about BetterScan and our ICO
+      </div>
+      <div className="faq-questions-list-container">
+        <div
+          onClick={handleQuestionClick}
+          className="faq-questions-first-question-container"
+        >
+          <div className="faq-questions-first-question-title-and-chevron">
+            <div className="faq-questions-first-question-title">
+              Sur quelle blockchain sont les $BSCAN ?
+            </div>
+            <div>
+              <img src={chevronUp} alt="chevron logo" />
+            </div>
+          </div>
+          <div
+            style={isFirstQuestionClicked ? {} : { display: "none" }}
+            className="faq-questions-first-question-answer"
+          >
+            Le jeton est un ERC-20 sur la blockchain ZKSync Era. Pour ajouter le
+            smart-contract à ton Metamask : 0x...
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default FAQ
+export default FAQ;
